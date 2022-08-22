63a4be1a9b7925e61e601c188bd922956f2f5070
sakthi-lucia0567 <sakthivel8725@outlook.com>
2022-08-22 10:00:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+function formValid() {
+  let name = document.getElementById("name");
+  let nameEl = name.value;
+  let password = document.getElementById("password");
+  let passwordEl = password.value;
+
+  console.log(nameEl, passwordEl);
+}
+const name = document.getElementById("name");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const errorElement = document.getElementById("error");
+
+form.addEventListener("submit", (e) => {
+  let messages = [];
+  if (name.value === "" || name.value == null) {
+    messages.push("Name is required");
+  }
+
+  if (password.value.length <= 6) {
+    messages.push("Password must be longer than 6 characters");
+  }
+
+  if (password.value.length >= 20) {
+    messages.push("Password must be less than 20 characters");
+  }
+
+  if (password.value === "password") {
+    messages.push("Password cannot be password");
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join(", ");
+  }
+});
